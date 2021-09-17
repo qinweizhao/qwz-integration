4761598adcf3401f90014e0700a569cf5eb9be76
YVKG <yvkg@qq.com>
2021-09-17 20:01:13 +0800

    commit

--- boot-config/src/main/java/com/qinweizhao/entity/Person.java
+++ boot-config/src/main/java/com/qinweizhao/entity/Person.java
@@ -1,6 +1,7 @@
 package com.qinweizhao.entity;
 
 import com.qinweizhao.support.YamlPropertySourceFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
@@ -21,13 +22,13 @@ import java.util.Map;
  */
 @Component
 @ConfigurationProperties(prefix = "person")
-@PropertySource(value = "classpath:person.yml",factory = YamlPropertySourceFactory.class )
 @Validated
 public class Person {
 
     /**
      * 姓名
      */
+    @Value("${police.name}")
     private String name;
 
     /**

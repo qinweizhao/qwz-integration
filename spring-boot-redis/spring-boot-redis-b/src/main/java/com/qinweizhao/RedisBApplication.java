663700b4c9d5423abd952042c58de094befc7195
YVKG <yvkg@qq.com>
2021-10-19 19:52:17 +0800

    commit

--- spring-boot-redis/spring-boot-redis-b/src/main/java/com/qinweizhao/RedisBApplication.java
+++ spring-boot-redis/spring-boot-redis-b/src/main/java/com/qinweizhao/RedisBApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootRedisBApplication {
+public class RedisBApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootRedisBApplication.class, args);
+        SpringApplication.run(RedisBApplication.class, args);
     }
 
 }

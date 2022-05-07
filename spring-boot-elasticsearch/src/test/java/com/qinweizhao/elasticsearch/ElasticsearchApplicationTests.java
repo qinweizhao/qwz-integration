2929b60f19f6f239b3e0e41ff2c54e5c7a2dfd51
YVKG <yvkg@qq.com>
2022-05-07 23:18:13 +0800

    commit

--- spring-boot-elasticsearch/src/test/java/com/qinweizhao/elasticsearch/ElasticsearchApplicationTests.java
+++ spring-boot-elasticsearch/src/test/java/com/qinweizhao/elasticsearch/ElasticsearchApplicationTests.java
@@ -1,6 +1,10 @@
 package com.qinweizhao.elasticsearch;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import com.qinweizhao.elasticsearch.entity.Account;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +12,49 @@ class ElasticsearchApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+
+    @Autowired
+    ElasticsearchClient elasticsearchClient;
+
+//    GET bank/_search
+//    {
+//        "query": {
+//        "match": {
+//            "address": "mill"
+//        }
+//    },
+//        "size": 0,
+//            "aggs": {
+//        "group_by_age": {
+//            "terms": {
+//                "field": "age"
+//            }
+//        },
+//        "avg_age":{
+//            "avg": {
+//                "field": "age"
+//            }
+//        }
+//    }
+//    }
+
+    @Test
+    void searchData() throws Exception {
+
+        String qs = "mill";
+        SearchResponse<Account> search = elasticsearchClient.search(s -> s.index("bank").query(q ->
+                q.match(m ->
+                        m.field("address").query(qs)
+                )
+        ).size(10).from(1), Account.class);
+
+
+        assert search.hits().total() != null;
+        long value = search.hits().total().value();
+
+        System.out.println(value);
     }
 
 }

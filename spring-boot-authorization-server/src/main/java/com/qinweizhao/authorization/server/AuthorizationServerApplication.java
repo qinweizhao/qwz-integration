ad4f5d8ebbeed8717e7f50491f2cadcfce6f4cdf
YVKG <yvkg@qq.com>
2022-11-19 22:48:13 +0800

    commit

--- spring-boot-authorization-server/src/main/java/com/qinweizhao/authorization/server/AuthorizationServerApplication.java
+++ spring-boot-authorization-server/src/main/java/com/qinweizhao/authorization/server/AuthorizationServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootAuthorizationServerApplication {
+public class AuthorizationServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootAuthorizationServerApplication.class, args);
+        SpringApplication.run(AuthorizationServerApplication.class, args);
     }
 
 }
